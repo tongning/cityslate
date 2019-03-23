1a4584a29bee91651bf7f196ee14aae59884374e
Anthony Li <antli.inbox@gmail.com>
2019-03-23 11:20:13 -0400

    Remove extra buttons

--- screens/QuestionsScreen.js
+++ screens/QuestionsScreen.js
@@ -35,15 +35,10 @@ export default class QuestionsScreen extends React.Component {
         </AwesomeButtonBlue>
 
         <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+          <ActionButton.Item buttonColor='#9b59b6' title="Add Question" onPress={() => console.log("notes tapped!")}>
             <Icon name="md-create" style={styles.actionButtonIcon} />
           </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-            <Icon name="md-done-all" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
+
         </ActionButton>
       </View>
     );
