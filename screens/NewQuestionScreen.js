ebbb30b38c270c44f8929af57e1f7048b6deb87b
Anthony Li <antli.inbox@gmail.com>
2019-03-23 12:36:42 -0400

    Add more to form

--- screens/NewQuestionScreen.js
+++ screens/NewQuestionScreen.js
@@ -7,36 +7,70 @@ import MapScreen from '../screens/MapScreen';
 import QuestionScreen from '../screens/QuestionScreen';
 import firebase from '../firebase.js'; // <--- add this line
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
-
+import GenerateForm from 'react-native-form-builder';
 
 export default class NewQuestionScreen extends React.Component {
-    
+    login() {
+        const formValues = this.formGenerator.getValues();
+        console.log('FORM VALUES', formValues);
+      }
     render() {
         //this.writeUserData("A","B","C");
         return (
-            <View>
-              
-              <Text>Form goes here</Text>
-                
-            </View>
+            <View style={formStyles.wrapper}>
+        <View>
+          <GenerateForm
+            ref={(c) => {
+              this.formGenerator = c;
+            }}
+            fields={fields}
+          />
+        </View>
+        <View style={formStyles.submitButton}>
+          <Button block title="Login" onPress={() => this.login()}>
+            <Text>Login</Text>
+          </Button>
+        </View>
+      </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-  submitButton: {
-  width: 60,  
-  height: 60,   
-  borderRadius: 30,            
-  backgroundColor: '#ee6e73',                                    
-  position: 'absolute',                                          
-  bottom: 10,                                                    
-  right: 10, 
-}
-});
 
+
+
+const formStyles = {
+    wrapper: {
+      flex: 1,
+      marginTop: 150,
+    },
+    submitButton: {
+      paddingHorizontal: 10,
+      paddingTop: 20,
+    },
+  };
+  // These Fields will create a login form with three fields
+  const fields = [
+    {
+      type: 'text',
+      name: 'user_name',
+      required: true,
+      icon: 'ios-person',
+      label: 'Username',
+    },
+    {
+      type: 'password',
+      name: 'password',
+      icon: 'ios-lock',
+      required: true,
+      label: 'Password',
+    },
+    {
+      type: 'picker',
+      name: 'country',
+      mode: 'dialog',
+      label: 'Select Country',
+      defaultValue: 'INDIA',
+      options: ['US', 'INDIA', 'UK', 'CHINA', 'FRANCE'],
+    },
+  ];
