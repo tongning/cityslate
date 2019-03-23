7affff946e00234550d0f29ad77524bbfeebb21a
kjiang12 <7127959+kjiang12@users.noreply.github.com>
2019-03-23 01:03:36 -0400

    added Map View

---
+++ screens/MapScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { MapView } from 'expo';
+
+export default class MapScreen extends React.Component {
+  static navigationOptions = {
+    title: 'app.json',
+  };
+
+  render() {
+    /* Go ahead and delete ExpoConfigView and replace it with your
+     * content, we just wanted to give you a quick view of your config */
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    );
+  }
+}
