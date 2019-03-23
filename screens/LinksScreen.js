14b1e813a72189307324d28d64a8483aaf0379a9
Anthony Li <antli.inbox@gmail.com>
2019-03-23 01:04:35 -0400

    Remove questions not added in loop

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -26,12 +26,7 @@ export default class LinksScreen extends React.Component {
       <View style={{flex:1}}>
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-    
-           <HomePageQuestions my_comment = {"this is a comment"}></HomePageQuestions>
-           <HomePageQuestions my_comment = {"this is a comment"}></HomePageQuestions>
-           <HomePageQuestions my_comment = {"this is a comment"}></HomePageQuestions>
-           
+           * content, we just wanted to provide you with some helpful links */}   
             {this.createListOfStuff()}
            
         
