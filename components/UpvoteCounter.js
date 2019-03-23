0e5d7bfeabe4f3d46c67d486531bd48ee2125efc
angwhen <angelmwen@gmail.com>
2019-03-23 16:25:58 -0400

    updating upvotes updated

--- components/UpvoteCounter.js
+++ components/UpvoteCounter.js
@@ -9,12 +9,12 @@ export default class UpvoteCounter extends React.Component {
 
   render() {
       upvote_count =  0
-      my_comment = this.props_my_comment_prop
+      my_comment = this.props.my_comment_prop
       firebase.database().ref('Questions/').on('value', function (snapshot) {
         console.log("SNAP",snapshot.val())
         for (var key in snapshot.val()){
           
-       if (snapshot.val()[key].questionText ==  {my_comment}){
+       if (snapshot.val()[key].questionText ===  my_comment){
             upvote_count = snapshot.val()[key].upvotes
        }
     }
@@ -29,25 +29,27 @@ export default class UpvoteCounter extends React.Component {
           title={upvote_count.toString()}
          onPress={() => {
 
-         
-          /*var my_id;
-          firebase.database().ref('Questions/').on('value', function (snapshot) {
+          
+        
+          var my_id;
+          firebase.database().ref('Questions/').once('value', function (snapshot) {
             console.log("SNAP",snapshot.val())
             for (var key in snapshot.val()){
-              
-           if (snapshot.val()[key].questionText ==  {my_comment}){
+              console.log("CURRENT QUESTION TEXT IS ", snapshot.val()[key].questionText, my_comment)
+           if (snapshot.val()[key].questionText ===  my_comment){
                my_id = key
+               console.log("KEY IS", key)
            }
         }
-          });*/
+          });
 
-         /* var databaseRef = firebase.database().ref('Questions/').child(my_id).child('upvotes');
-
+          console.log("MY ID IS", my_id, my_comment)
+         var databaseRef = firebase.database().ref('Questions/').child(my_id).child('upvotes');
+          console.log("Data base ref is ", databaseRef)
           databaseRef.transaction(function(upvotes) {
-            if (upvotes || 0) {
-              upvotes = upvotes +1;
-            }
-          });*/
+            console.log("want to UPDATING UPVOTES!")
+            return (upvotes || 0) + 1;
+          });
          }}
          
          />
