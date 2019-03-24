919dfb514ee35cb1dee88b35ef3addca1e1caf4a
Justin Chao <justinchao20@gmail.com>
2019-03-23 23:49:40 -0400

    making things better

--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -18,7 +18,7 @@ export default class QuestionScreen extends React.Component {
       <View style={{flex:1}}>
      <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
      <Text style={{fontSize: 20}} >HELLOW {this.props.navigation.state.params.myItemName} </Text>
-     <Button  
+     <Button
            color="#ff5c5c"
           title="GO BACK"
          onPress={() => this.props.navigation.goBack(null)}
