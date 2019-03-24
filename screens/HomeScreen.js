188c5a150be5e09062451cc65da5e7a9b958c682
Justin Chao <justinchao20@gmail.com>
2019-03-24 06:41:31 -0400

    done now

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -89,7 +89,7 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 50,
-    fontFamily:'sans-serif',
+    fontFamily:'monospace',
     color: 'lightblue',
     marginBottom: 20,
   },
@@ -133,34 +133,3 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
   }
 });
-
-
-// import React, { Component } from 'react';
-// import {
-//     ScrollView,
-//     Text,
-//     TextInput,
-//     View,
-//     Button
-// } from 'react-native';
-
-// export default class Login extends Component {
-
-//     render() {
-//         return (
-//             <ScrollView style={{padding: 20}}>
-//                 <Text 
-//                     style={{fontSize: 27}}>
-//                     Login
-//                 </Text>
-//                 <TextInput placeholder='Username' />
-//                 <TextInput placeholder='Password' />
-//                 <View style={{margin:7}} />
-//                 <Button 
-//                           onPress={this.props.onLoginPress}
-//                           title="Submit"
-//                       />
-//                   </ScrollView>
-//             )
-//     }
-// }
